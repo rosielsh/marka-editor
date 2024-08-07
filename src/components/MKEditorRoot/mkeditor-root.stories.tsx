856fa0ff98c9c9ef10486ed28a260e616b15e3dd
suhwa <tnghk9611@naver.com>
2024-08-07 20:00:03 +0900

    feat #20: create story for Editor.Root component

---
+++ src/components/MKEditorRoot/mkeditor-root.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import MKEditorTextarea from '../MKEditorTextArea/mkeditor-textarea';
+import MKEditorRoot, { IRootProps } from './mkeditor-root';
+
+const meta: Meta<typeof MKEditorRoot> = {
+  title: 'MKEditor/MKEditorRoot',
+  component: MKEditorRoot,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof MKEditorRoot>;
+
+export const Default: Story = {
+  render: (args: IRootProps) => (
+    <MKEditorRoot {...args}>
+      <div>Default Child Component</div>
+    </MKEditorRoot>
+  ),
+};
+
+export const CustomContent: Story = {
+  render: (args: IRootProps) => (
+    <MKEditorRoot {...args}>
+      <h2>Custom Heading</h2>
+      <p>This is some custom content inside MKEditorRoot.</p>
+    </MKEditorRoot>
+  ),
+};
+
+export const WithTextArea: Story = {
+  render: (args: IRootProps) => (
+    <MKEditorRoot {...args}>
+      <MKEditorTextarea />
+    </MKEditorRoot>
+  ),
+};
