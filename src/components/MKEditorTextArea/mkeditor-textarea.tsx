03f968bbe25ceb89ab88454e35da8a753df2aef5
suhwa <tnghk9611@naver.com>
2024-08-07 20:01:46 +0900

    feat #20: create textarea component for temporary verification

--- src/components/MKEditorTextArea/mkeditor-textarea.tsx
+++ src/components/MKEditorTextArea/mkeditor-textarea.tsx
@@ -1,5 +1,13 @@
+import { useEditor } from '../utils/useEditor';
+
 const MKEditorTextarea = () => {
-  return <div>mkeditor-textarea</div>;
+  const { markdownText, updateMarkdownText } = useEditor();
+  return (
+    <div>
+      <textarea value={markdownText} onChange={e => updateMarkdownText(e.target.value)} placeholder="write here.." />
+      <p>preview : {markdownText}</p>
+    </div>
+  );
 };
 
 export default MKEditorTextarea;
