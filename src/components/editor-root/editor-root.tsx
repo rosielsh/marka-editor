f356886e5f70a066b1f41d69c1755a9e06619e06
suhwa <tnghk9611@naver.com>
2024-08-19 00:14:12 +0900

    refactor #23: Modify the props so that other props can also be received

--- src/components/editor-root/editor-root.tsx
+++ src/components/editor-root/editor-root.tsx
@@ -1,14 +1,17 @@
-import useEditorState from '@/hooks/useEditorState';
 import EditorRootContextProvider from '@/providers/EditorRootContextProvider';
+import React from 'react';
 
-export interface EditorRootProps {
+export interface EditorRootProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
+  initialContent?: string;
 }
 
-const EditorRoot = ({ children }: EditorRootProps) => {
-  const editorState = useEditorState();
-
-  return <EditorRootContextProvider value={editorState}>{children}</EditorRootContextProvider>;
+const EditorRoot = ({ children, initialContent = '', ...props }: EditorRootProps) => {
+  return (
+    <EditorRootContextProvider initialContent={initialContent}>
+      <div {...props}>{children}</div>
+    </EditorRootContextProvider>
+  );
 };
 
 export default EditorRoot;
