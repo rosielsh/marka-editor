195e1bee568d4dffb452c38a2ad7ae0b6dd5654f
suhwa <tnghk9611@naver.com>
2024-08-28 04:32:19 +0900

    fix: Delete gitignore

---
+++ packages/marka-editor/storybook-static/assets/editor-content.stories-CpDqpuZe.js
@@ -0,0 +1,45 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import{E as D}from"./EditorRootContextProvider-B_5zPgiO.js";import{E as a}from"./editor-content-B7sPVZeR.js";import"./index-DJO9vBfz.js";const H={title:"MKEditor/EditorContent",component:a,argTypes:{readOnly:{control:"boolean"},disabled:{control:"boolean"},maxLength:{control:"number"},placeholder:{control:"text"},required:{control:"boolean"},onFocusOutside:{action:"focusedOutside"},onFocusInside:{action:"focusedInside"},onValueChange:{action:"valueChanged"}},decorators:[r=>e.jsx(D,{children:e.jsx(r,{})})],tags:["autodocs"]},o={render:r=>e.jsx(a,{...r}),args:{placeholder:"Start writing..."}},s={render:r=>e.jsx(a,{...r}),args:{readOnly:!0}},t={render:r=>e.jsx(a,{...r}),args:{disabled:!0}},n={render:r=>e.jsx(a,{...r}),args:{maxLength:10,placeholder:"Max 10 characters"}},d={render:r=>e.jsx(a,{...r}),args:{required:!0}},c={render:r=>e.jsx(a,{...r}),args:{placeholder:"Custom placeholder"}},i={render:r=>e.jsx(a,{...r}),args:{style:{width:"100%",height:"200px",border:"1px solid #007bff",borderRadius:"5px",padding:"10px",fontSize:"16px"},placeholder:"Custom styled textarea"}};var l,p,u;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => <EditorContent {...args} />,
+  args: {
+    placeholder: 'Start writing...'
+  }
+}`,...(u=(p=o.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var m,g,x;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: args => <EditorContent {...args} />,
+  args: {
+    readOnly: true
+  }
+}`,...(x=(g=s.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};var h,C,b;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => <EditorContent {...args} />,
+  args: {
+    disabled: true
+  }
+}`,...(b=(C=t.parameters)==null?void 0:C.docs)==null?void 0:b.source}}};var E,f,S;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: args => <EditorContent {...args} />,
+  args: {
+    maxLength: 10,
+    placeholder: 'Max 10 characters'
+  }
+}`,...(S=(f=n.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var y,j,O;d.parameters={...d.parameters,docs:{...(y=d.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: args => <EditorContent {...args} />,
+  args: {
+    required: true
+  }
+}`,...(O=(j=d.parameters)==null?void 0:j.docs)==null?void 0:O.source}}};var R,W,q;c.parameters={...c.parameters,docs:{...(R=c.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: args => <EditorContent {...args} />,
+  args: {
+    placeholder: 'Custom placeholder'
+  }
+}`,...(q=(W=c.parameters)==null?void 0:W.docs)==null?void 0:q.source}}};var L,M,w;i.parameters={...i.parameters,docs:{...(L=i.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: args => <EditorContent {...args} />,
+  args: {
+    style: {
+      width: '100%',
+      height: '200px',
+      border: '1px solid #007bff',
+      borderRadius: '5px',
+      padding: '10px',
+      fontSize: '16px'
+    },
+    placeholder: 'Custom styled textarea'
+  }
+}`,...(w=(M=i.parameters)==null?void 0:M.docs)==null?void 0:w.source}}};const I=["Default","ReadOnly","Disabled","WithMaxLength","Required","WithCustomPlaceHolder","WithCustomStyles"];export{o as Default,t as Disabled,s as ReadOnly,d as Required,c as WithCustomPlaceHolder,i as WithCustomStyles,n as WithMaxLength,I as __namedExportsOrder,H as default};
